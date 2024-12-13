20d1d354debfd922378343989d953836ef3ceb27
Sharayu-A <sharayuadikane03@gmail.com>
2024-12-13 17:12:24 +0530

    Update new.py

--- new.py
+++ new.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title(" Interactive Air Quality Insights")
 st.markdown(
     """
-    <iframe width="100%" height="400" src="https://app.powerbi.com/view?r=eyJrIjoiNGQ3NTZkY2ItNDZhYi00MjMyLWI3ZGQtODdhMzc2NzRhNDUxIiwidCI6IjkzZTljMTgyLTdhOWMtNGI4YS04YzY1LTM3OTMyNDZlYzgzMyJ9" frameborder="0" allowFullScreen="true"></iframe>
+    <iframe width="100%" height="400" src="https://app.powerbi.com/view?r=eyJrIjoiMGQ5NjI0YjItNjc2ZC00ZDRlLThjMmYtNjc2ZDdhODdiNzYyIiwidCI6IjkzZTljMTgyLTdhOWMtNGI4YS04YzY1LTM3OTMyNDZlYzgzMyJ9" frameborder="0" allowFullScreen="true"></iframe>
     """,
     unsafe_allow_html=True,
 )
